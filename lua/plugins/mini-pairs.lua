b263853e328ce181d73d97afea8887f49a0d9738
vgont <vgontdev@gmail.com>
2025-01-26 21:06:44 -0300

    feat: mini-pairs

---
+++ lua/plugins/mini-pairs.lua
@@ -0,0 +1,9 @@
+return {
+  "echasnovski/mini.pairs",
+  event = "InsertEnter",
+  opts = {
+    disable_in_visualblock = true,
+    disable_in_insert = true,
+    disable_in_replace = true,
+  },
+}
