d6b5e9f75ddddb01785c6267565255863da909a2
vgont <vgontdev@gmail.com>
2025-01-28 13:55:52 -0300

    feat (init.lua): add transparent background; set signcolumn; keymap for close buffer

--- init.lua
+++ init.lua
@@ -4,12 +4,24 @@ vim.cmd("set expandtab")
 vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
+vim.cmd [[
+  highlight Normal guibg=none
+  highlight NonText guibg=none
+  highlight Normal ctermbg=none
+  highlight NonText ctermbg=none
+]]
 
-vim.keymap.set("n", "<leader>f", ":Pick files<CR>", {noremap=true, silent=true})
-vim.keymap.set("n", "<Tab>", ":Oil --float<CR>", {noremap=true, silent=true})
-
--- add a padding to the left of the line numbers
 vim.opt.numberwidth = 4
 vim.opt.number = true
 vim.opt.relativenumber = true
-vim.opt.signcolumn = "yes"
+
+vim.opt.signcolumn = "no"
+
+vim.keymap.set("n", "<leader>q", function()
+    if #vim.fn.getbufinfo({buflisted = 1}) <= 1 then
+        vim.cmd("Oil")
+    else
+        vim.cmd("bd")
+    end
+end)
+
