8daac909de9ebc3e48e553976f14fb481bcc51be
vgont <vgontdev@gmail.com>
2025-01-28 14:01:00 -0300

    feat (mini-pick): add keymaps for picking files and buffers

--- lua/plugins/mini-pick.lua
+++ lua/plugins/mini-pick.lua
@@ -6,5 +6,9 @@ return {
       move_up = '<C-k>',
       move_down = '<C-j>'
     }
+  },
+  keys = {
+    vim.keymap.set("n", "<leader>f", ":Pick files<CR>", {noremap=true, silent=true}),
+    vim.keymap.set("n", "<leader>b", ":Pick buffers<CR>", {noremap=true, silent=true}),
   }
 }
