56c22a4899957aa6c737e2b35a16634afbc73e10
vgont <vgontdev@gmail.com>
2025-01-28 14:02:51 -0300

    feat (oil): add keymap to open and select files in oil

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -3,9 +3,20 @@ return {
   name = "oil",
   dependencies = { { "echasnovski/mini.icons", opts = {} } },
   lazy = false,
+  keys = {
+    { "<leader><Tab>", "<cmd>Oil --float<CR>", desc = "Open Oil -- Float" },
+  },
   opts = {
+    default_file_explorer = true,
     keymaps = {
-      ["<Tab>"] = "actions.select"
+      ["<Tab>"] = "actions.select",
+    },
+    view_options = {
+      show_hidden = true,
+    },
+    confirmation = {
+      border = "none",
+      title = "Oil",
     },
     float = {
       preview_split = "right"
