c2cc5601f53d786817f9bed46c58ef6e8da39efd
vgont <vgontdev@gmail.com>
2025-02-07 19:46:50 -0300

    feat (lsp-config): add ts-ls, eslint, rust-analyzer, pyright

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -11,6 +11,10 @@ return {
       require('mason-lspconfig').setup({
         ensure_installed = {
           'lua_ls',
+          'ts_ls',
+          'eslint',
+          'rust_analyzer',
+          'pyright',
         }
       })
     end
@@ -19,7 +23,13 @@ return {
     'neovim/nvim-lspconfig',
     config = function()
       local lspconfig = require("lspconfig")
+
       lspconfig.lua_ls.setup({})
+      lspconfig.ts_ls.setup({})
+      lspconfig.eslint.setup({})
+      lspconfig.rust_analyzer.setup({})
+      lspconfig.pyright.setup({})
+
       vim.keymap.set('n', '<C-k>', vim.lsp.buf.hover, {})
       vim.keymap.set({'n', 'v'}, '<leader>ca', vim.lsp.buf.code_action, {})
     end
