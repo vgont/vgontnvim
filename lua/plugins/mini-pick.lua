f978e3f122f06f1c6a037d3ed638d17e88c5fe74
vgont <vgontdev@gmail.com>
2025-02-16 20:09:05 -0300

    feat (mini-pick): key to pick with git or fd

--- lua/plugins/mini-pick.lua
+++ lua/plugins/mini-pick.lua
@@ -8,7 +8,8 @@ return {
     }
   },
   keys = {
-    { "ff", "<cmd>Pick files<CR>", desc = "Open Pick files" },
+    { "<leader>f", "<cmd>Pick files tool='git'<CR>", desc = "Open Pick files with git" },
+    { "ff", "<cmd>Pick files tool='fd'<CR>", desc = "Open Pick files with fd" },
     { "fb", "<cmd>Pick buffers<CR>", desc = "Open Pick buffers" },
     { "fg", "<cmd>Pick grep<CR>", desc = "Open Pick buffers" },
   }
