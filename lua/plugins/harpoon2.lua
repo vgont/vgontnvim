be200c15fe38a5f17b773600883bee357ff2de19
vgont <vgontdev@gmail.com>
2025-07-10 17:33:20 -0300

    feat (harpoon2): rounded borders and new keymaps

--- lua/plugins/harpoon2.lua
+++ lua/plugins/harpoon2.lua
@@ -4,19 +4,28 @@ return {
   dependencies = {
     "nvim-lua/plenary.nvim",
   },
+
   config = function()
     local harpoon = require("harpoon")
     harpoon:setup()
 
     for i = 1, 9 do
-      vim.keymap.set('n', '<leader>h' .. i, function()
-        harpoon:list():select(i)
-        print('harpoon: selected ' .. i)
+      local keys = {'w', 'f', 'p', 'r', 's', 't', 'x', 'c', 'd'}
+
+      vim.keymap.set('n', '<leader>h' .. keys[i], function()
+	harpoon:list():select(i)
+	print('harpoon: selected ' .. i)
       end, { desc = 'harpoon: select ' .. i })
     end
 
+    local toggle_opts = {
+      border = "rounded",
+      title_pos = "center",
+      title = " Harpoon ",
+    }
+
     vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end)
-    vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
+    vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list(), toggle_opts) end)
   end
 }
 
