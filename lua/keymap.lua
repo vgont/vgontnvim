49292ca6c20aa6f6a5ba10f65c78185d0546f1b3
vgont <vgontdev@gmail.com>
2025-07-13 18:47:27 -0300

    feat: more keymaps (good ones this time)

--- lua/keymap.lua
+++ lua/keymap.lua
@@ -3,6 +3,12 @@ vim.keymap.set('n', '<Tab>', ':bnext<CR>')
 vim.keymap.set('n', '<S-Tab>', ':bprev<CR>')
 vim.keymap.set('n', '<leader>q', ':bd<CR>')
 
+-- Good ones
+vim.keymap.set('n', '<C-d>', '<C-d>zz')
+vim.keymap.set('n', '<C-u>', '<C-u>zz')
+vim.keymap.set('n', 'n', 'nzzzv')
+vim.keymap.set('n', 'N', 'Nzzzv')
+
 -- Just Insanity
 vim.keymap.set('n', '<leader>O', 'mzO<Esc>`z<cmd>delm z<CR>', { desc = "Add blank line above" })
 vim.keymap.set('n', '<leader>o', 'mzo<Esc>`z<cmd>delm z<CR>', { desc = "Add blank line below" })
