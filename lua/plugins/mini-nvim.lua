5501f80caa2000cc3fd44d33f801d74e19dbe01a
vgont <vgontdev@gmail.com>
2025-07-13 20:34:52 -0300

    refactor (mini): remove unused plugins

--- lua/plugins/mini-nvim.lua
+++ lua/plugins/mini-nvim.lua
@@ -18,9 +18,7 @@ return {
     require("mini.operators").setup()
     require("mini.indentscope").setup()
     require("mini.bracketed").setup()
-    require("mini.jump").setup()
     require("mini.statusline").setup()
-    require("mini.splitjoin").setup()
     require("mini.diff").setup()
     require("mini.git").setup()
 
@@ -37,12 +35,6 @@ return {
       },
     })
 
-    require("mini.pairs").setup({
-      disable_in_visualblock = true,
-      disable_in_insert = true,
-      disable_in_replace = true,
-    })
-
     require("mini.pick").setup({
       window = {
         config = { border = "rounded" }
@@ -50,9 +42,9 @@ return {
     })
   end,
   keys = {
-    { "<leader>pf", "<cmd>Pick files<CR>",                    desc = "files picker" },
+    { "<leader>pf", "<cmd>Pick files<CR>",                    desc = "file picker" },
     { "<leader>pg", "<cmd>Pick grep_live<CR>",                desc = "grep picker" },
-    { "<leader>pb", "<cmd>Pick buffers<CR>",                  desc = "buffers picker" },
+    { "<leader>pb", "<cmd>Pick buffers<CR>",                  desc = "buffer picker" },
     { "<leader>gc", "<cmd>Git commit<CR>",                    desc = "git commit" },
     { "<leader>gt", "<cmd>Git status<CR>",                    desc = "git status" },
     { "<leader>gs", "<cmd>lua MiniGit.show_at_cursor()<CR>",  desc = "git related data" },
