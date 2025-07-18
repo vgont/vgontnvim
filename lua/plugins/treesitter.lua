356bd16732cf93ed63fac9fe428aafd470ead493
vgont <vgontdev@gmail.com>
2025-07-17 22:07:24 -0300

    feat (treesitter): ignore treesitter in large files

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -24,7 +24,18 @@ return {
       'yaml',
     },
     auto_install = false,
-    highlight = { enable = true },
+    highlight = {
+      enable = true,
+      disable = function(lang, buf)
+        if lang == 'json' then
+          local max_filesize = 500 * 1024 -- 100 KB
+          local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+          if ok and stats and stats.size > max_filesize then
+            return true
+          end
+        end
+      end,
+    },
     indent = { enable = true },
     incremental_selection = {
       enable = true,
