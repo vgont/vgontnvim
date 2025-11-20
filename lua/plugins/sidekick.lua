8a78a07030e490db47a3e2e9801cbc0acc1aea1f
vgont <vgontdev@gmail.com>
2025-11-20 13:19:29 -0300

    refactor (sidekick): remove NES

--- lua/plugins/sidekick.lua
+++ lua/plugins/sidekick.lua
@@ -11,17 +11,6 @@ return {
   },
   keys = {
     {
-      "<tab>",
-      function()
-        -- if there is a next edit, jump to it, otherwise apply it if any
-        if not require("sidekick").nes_jump_or_apply() then
-          return "<Tab>" -- fallback to normal tab
-        end
-      end,
-      expr = true,
-      desc = "Goto/Apply Next Edit Suggestion",
-    },
-    {
       "<c-.>",
       function() require("sidekick.cli").toggle() end,
       desc = "Sidekick Toggle",
@@ -71,4 +60,7 @@ return {
       desc = "Sidekick Toggle Copilot",
     },
   },
+  init = function()
+    require("sidekick.nes").disable()
+  end,
 }
