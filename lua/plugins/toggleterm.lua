639109998c35941b305c8037a8493a1d024f8f00
vgont <vgontdev@gmail.com>
2025-12-31 13:52:42 -0300

    feat: add toggleterm keys

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -5,12 +5,16 @@ return {
     open_mapping = [[<c-\>]],
     hide_numbers = false,
     direction = 'float',
+    width = 0.40,
+    float_opts = {
+      border = 'curved',
+    },
   },
   config = function(_, opts)
     require('toggleterm').setup(opts)
-    local dir = vim.fn.expand('%:p:h'):gsub(vim.env.HOME, '~'):gsub('oil://', '')
+    local dir = function () vim.fn.expand('%:p:h'):gsub(vim.env.HOME, '~'):gsub('oil://', '') end
     vim.keymap.set("x", "<leader>tv", function() vim.cmd("'<,'>ToggleTermSendVisualSelection") end)
-    vim.keymap.set("n", "<leader>tf", function() vim.cmd("ToggleTerm dir=" .. dir .. " name=" .. dir) end)
+    vim.keymap.set("n", "<leader>tf", function() vim.cmd("ToggleTerm dir=" .. dir() .. " name=" .. dir()) end)
     vim.keymap.set("n", "<leader>tt", function() vim.cmd("ToggleTerm direction=horizontal") end)
   end
 }
