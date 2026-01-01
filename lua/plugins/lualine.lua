81b7b786c2eb24716528920d1f0fc4d536a04d28
vgont <vgontdev@gmail.com>
2026-01-01 02:08:42 -0300

    style: lualine

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -3,11 +3,17 @@ return {
   dependencies = { 'nvim-tree/nvim-web-devicons' },
   opts = {
     options = {
-      component_separators = { left = '', right = '' },
+      component_separators = { left = '|', right = '' },
       section_separators = { left = '', right = '' },
     },
     sections = {
-      lualine_b = { "grapple" }
+      lualine_a = { "mode" },
+      lualine_b = { "branch", "diagnostics" },
+      lualine_c = { "filename" },
+
+      lualine_x = { "fileformat", "filetype" },
+      lualine_y = { "lsp_status" },
+      lualine_z = {}
     }
   },
 }
