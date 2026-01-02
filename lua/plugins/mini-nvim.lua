e8453b5fd9ea6f4ebdbc723bc65102cd13b35fa3
vgont <vgontdev@gmail.com>
2026-01-01 22:34:15 -0300

    refactor (mini-move): keymaps

--- lua/plugins/mini-nvim.lua
+++ lua/plugins/mini-nvim.lua
@@ -17,23 +17,11 @@ return {
     require("mini.surround").setup()
     require("mini.diff").setup()
     require("mini.git").setup()
+    require("mini.move").setup()
 
     require("mini.operators").setup({
       replace = {
         prefix = "",
-      },
-    })
-
-    require("mini.move").setup({
-      mappings = {
-        left = "<M-left>",
-        right = "<M-right>",
-        down = "<M-down>",
-        up = "<M-up>",
-        line_left = "<M-left>",
-        line_right = "<M-right>",
-        line_down = "<M-down>",
-        line_up = "<M-up>",
       },
     })
 
@@ -43,22 +31,21 @@ return {
         use_cache = true
       }
     })
-
     MiniPick.registry.pick_current_dir = function()
       local dir = vim.fn.expand('%:p:h')
       return MiniPick.builtin.files({}, { source = { cwd = dir } })
     end
   end,
   keys = {
-    { "<leader>gc", "<cmd>Git commit<CR>",                       desc = "git commit" },
-    { "<leader>gt", "<cmd>Git status<CR>",                       desc = "git status" },
-    { "<leader>gs", "<cmd>lua MiniGit.show_at_cursor()<CR>",     desc = "git related data" },
-    { "<leader>go", "<cmd>lua MiniDiff.toggle_overlay()<CR>",    desc = "toggle git diff" },
+    { "<leader>gc", "<cmd>Git commit<CR>",                               desc = "git commit" },
+    { "<leader>gt", "<cmd>Git status<CR>",                               desc = "git status" },
+    { "<leader>gs", "<cmd>lua MiniGit.show_at_cursor()<CR>",             desc = "git related data" },
+    { "<leader>go", "<cmd>lua MiniDiff.toggle_overlay()<CR>",            desc = "toggle git diff" },
 
-    { "<leader>pf", "<cmd>lua MiniPick.builtin.files()<CR>",     desc = "pick files" },
+    { "<leader>pf", "<cmd>lua MiniPick.builtin.files()<CR>",             desc = "pick files" },
     { "<leader>pp", "<cmd>lua MiniPick.registry.pick_current_dir()<CR>", desc = 'pick current directory' },
-    { "<leader>pb", "<cmd>lua MiniPick.builtin.buffers()<CR>",   desc = 'pick buffers' },
-    { "<leader>pg", "<cmd>lua MiniPick.builtin.grep_live()<CR>", desc = 'live grep' },
-    { "<leader>p.", "<cmd>lua MiniPick.builtin.resume()<CR>",    desc = 'resume last picker' },
+    { "<leader>pb", "<cmd>lua MiniPick.builtin.buffers()<CR>",           desc = 'pick buffers' },
+    { "<leader>pg", "<cmd>lua MiniPick.builtin.grep_live()<CR>",         desc = 'live grep' },
+    { "<leader>p.", "<cmd>lua MiniPick.builtin.resume()<CR>",            desc = 'resume last picker' },
   },
 }
