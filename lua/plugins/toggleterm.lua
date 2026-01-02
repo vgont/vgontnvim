f5b95873f54283690b82ced1f32ea14b91b0863f
vgont <vgontdev@gmail.com>
2026-01-01 22:33:40 -0300

    refactor (toggleterm): keymaps

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -5,16 +5,18 @@ return {
     open_mapping = [[<c-\>]],
     hide_numbers = false,
     direction = 'float',
-    width = 0.40,
+    size = vim.o.columns * 0.4,
     float_opts = {
       border = 'curved',
     },
   },
   config = function(_, opts)
     require('toggleterm').setup(opts)
-    local dir = function () vim.fn.expand('%:p:h'):gsub(vim.env.HOME, '~'):gsub('oil://', '') end
-    vim.keymap.set("x", "<leader>tv", function() vim.cmd("'<,'>ToggleTermSendVisualSelection") end)
-    vim.keymap.set("n", "<leader>tf", function() vim.cmd("ToggleTerm dir=" .. dir() .. " name=" .. dir()) end)
-    vim.keymap.set("n", "<leader>tt", function() vim.cmd("ToggleTerm direction=horizontal") end)
+    local getCurrDir = function() return vim.fn.expand('%:p:h'):gsub(vim.env.HOME, '~'):gsub('oil://', '') end
+
+    vim.keymap.set("x", "<leader>ts", function() vim.cmd("'<,'>ToggleTermSendVisualSelection") end)
+    vim.keymap.set("n", "<leader>tf",
+      function() vim.cmd("ToggleTerm dir=" .. getCurrDir() .. " name=" .. getCurrDir()) end)
+    vim.keymap.set("n", "<leader>tv", function() vim.cmd("ToggleTerm direction=vertical") end)
   end
 }
