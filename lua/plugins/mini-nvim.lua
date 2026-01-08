7b374363fd5adf3b4b16d960a598fdc7841b51c9
vgont <vgontdev@gmail.com>
2026-01-08 03:27:09 -0300

    refactor: Change blink-cmp to mini-completion, add mini-cmdline

--- lua/plugins/mini-nvim.lua
+++ lua/plugins/mini-nvim.lua
@@ -12,12 +12,14 @@ return {
     vim.notify = require("mini.notify").make_notify()
   end,
   config = function()
+    vim.opt.pumheight = 5 -- Limit popup menu height to 5 items
     require("mini.ai").setup()
     require("mini.icons").setup()
     require("mini.surround").setup()
     require("mini.diff").setup()
     require("mini.git").setup()
     require("mini.move").setup()
+    require("mini.cmdline").setup()
 
     require("mini.operators").setup({
       replace = {
@@ -35,17 +37,34 @@ return {
       local dir = vim.fn.expand('%:p:h'):gsub(vim.env.HOME, '~'):gsub('oil://', '')
       return MiniPick.builtin.files({}, { source = { cwd = dir } })
     end
+
+    local MiniCompletion = require("mini.completion")
+    MiniCompletion.setup({ delay = { completion = 15 } })
   end,
   keys = {
-    { "<leader>gc", "<cmd>Git commit<CR>",                               desc = "git commit" },
-    { "<leader>gt", "<cmd>Git status<CR>",                               desc = "git status" },
-    { "<leader>gs", "<cmd>lua MiniGit.show_at_cursor()<CR>",             desc = "git related data" },
-    { "<leader>go", "<cmd>lua MiniDiff.toggle_overlay()<CR>",            desc = "toggle git diff" },
+    { "<leader>gc", "<cmd>Git commit<CR>",                    desc = "git commit",       mode = { "n" } },
+    { "<leader>gt", "<cmd>Git status<CR>",                    desc = "git status",       mode = { "n" } },
+    { "<leader>gs", "<cmd>lua MiniGit.show_at_cursor()<CR>",  desc = "git related data", mode = { "n" } },
+    { "<leader>go", "<cmd>lua MiniDiff.toggle_overlay()<CR>", desc = "toggle git diff",  mode = { "n" } },
+    {
+      "<leader>gr",
+      function()
+        local current_line = vim.fn.line(".")
+        vim.cmd('noau normal! y')
+        local selected_text = vim.fn.getreg('"')
+        vim.cmd('Git restore ' .. selected_text)
+        vim.cmd("bd!")
+        vim.cmd("Git status")
+        vim.fn.cursor(current_line, 1)
+      end,
+      desc = "git restore selected file",
+      mode = { "x" }
+    },
 
-    { "<leader>pf", "<cmd>lua MiniPick.builtin.files()<CR>",             desc = "pick files" },
-    { "<leader>pp", "<cmd>lua MiniPick.registry.pick_current_dir()<CR>", desc = 'pick current directory' },
-    { "<leader>pb", "<cmd>lua MiniPick.builtin.buffers()<CR>",           desc = 'pick buffers' },
-    { "<leader>pg", "<cmd>lua MiniPick.builtin.grep_live()<CR>",         desc = 'live grep' },
-    { "<leader>p.", "<cmd>lua MiniPick.builtin.resume()<CR>",            desc = 'resume last picker' },
+    { "<leader>pf", "<cmd>lua MiniPick.builtin.files()<CR>",             desc = "pick files",             mode = { "n" } },
+    { "<leader>pp", "<cmd>lua MiniPick.registry.pick_current_dir()<CR>", desc = 'pick current directory', mode = { "n" } },
+    { "<leader>pb", "<cmd>lua MiniPick.builtin.buffers()<CR>",           desc = 'pick buffers',           mode = { "n" } },
+    { "<leader>pg", "<cmd>lua MiniPick.builtin.grep_live()<CR>",         desc = 'live grep',              mode = { "n" } },
+    { "<leader>p.", "<cmd>lua MiniPick.builtin.resume()<CR>",            desc = 'resume last picker',     mode = { "n" } },
   },
 }
